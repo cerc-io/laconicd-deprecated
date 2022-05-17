f705908614533dcad7596306dafad4a762e2efba
Sai Kumar <sai@vitwit.com>
2022-05-17 07:20:21 +0530

    fix : fix the unit test  for modules

--- server/config/config_test.go
+++ server/config/config_test.go
@@ -9,6 +9,6 @@ import (
 func TestDefaultConfig(t *testing.T) {
 	cfg := DefaultConfig()
 	require.True(t, cfg.JSONRPC.Enable)
-	// require.Equal(t, cfg.JSONRPC.Address, DefaultJSONRPCAddress)
+	require.Equal(t, cfg.JSONRPC.Address, DefaultJSONRPCAddress)
 	require.Equal(t, cfg.JSONRPC.WsAddress, DefaultJSONRPCWsAddress)
 }
