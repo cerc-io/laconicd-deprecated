25d1577ce43e222f11411bc6ffd19a27c47827c9
Michael Shaw <michael@abastionofsanity.com>
2023-01-25 15:05:48 -0500

    utils/json test for comparing known, but deprecated method to new implementation

---
+++ utils/json_test.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAndValidateCIDGeneration(t *testing.T) {
+	testCases := []struct {
+		name    string
+		content string
+	}{
+		{
+			"empty string", "",
+		},
+		{
+			"empty json", "{}",
+		},
+
+		{
+			"test record", "\\xa6curlohttps://cerc.iodtypex\\x19WebsiteRegistrationRecordgversione0.0.1ltls_cert_cidx.QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnRrbuild_artifact_cidx.QmP8jTG1m9GSDJLCbeWhVSVgEzCPPwXRdCRuJtQ5Tz9Kc9x\\x1crepo_registration_record_cidx.QmSnuWmxptJZdLJpKRarxBMS2Ju2oANVrgbr2xWbie9b2D",
+		},
+	}
+
+	for _, tc := range testCases {
+		deprecatedAndCorrect, _ := CIDFromJSONBytes([]byte(tc.content))
+		newImpl, _ := CIDFromJSONBytesUsingIpldPrime([]byte(tc.content))
+		require.Equal(t, deprecatedAndCorrect, newImpl, tc.name)
+	}
+}
