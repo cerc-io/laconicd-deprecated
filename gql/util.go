fa938e76bb25b2e9a0df0c2c1eeac5bd8d815cd6
0xmuralik <krishna192reddy@gmail.com>
2023-03-17 11:37:13 +0530

    nosec g705

--- gql/util.go
+++ gql/util.go
@@ -170,6 +170,8 @@ func getReferences(ctx context.Context, resolver QueryResolver, r *registrytypes
 
 func getIds(obj map[string]interface{}) []string {
 	var ids []string
+
+	// #nosec G705
 	for key := range obj {
 		if innerObj, ok := obj[key].(map[string]interface{}); ok {
 			if _, ok := innerObj["/"]; ok && len(innerObj) == 1 {
